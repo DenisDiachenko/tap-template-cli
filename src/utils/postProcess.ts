c15a797a388cb0b5b43736d2872fad547a783724
DenysDiachenko <sarmat4ever@gmail.com>
2020-10-18 19:16:32 +0300

    updated template and postProccess util

--- src/utils/postProcess.ts
+++ src/utils/postProcess.ts
@@ -1,14 +1,30 @@
-import { cd, exec } from 'shelljs';
+import { cd, exec, which } from 'shelljs';
 import * as path from 'path';
 import * as fs from 'fs';
 
+import chalk from 'chalk';
+
 import { ICliOptions } from '../types'
+
+function postProcessNode(options: ICliOptions) {
+
+  let installationScript = which('yarn') ? 'yarn' : which('npm') ? 'npm i' : null;
+
+  if (installationScript) {
+    cd(options.targetPath);
+
+    console.log(chalk.bgCyan(`Installing modules by ${installationScript} ...`))
+    exec(installationScript);
+    console.log('');
+    exec('git init');
+  } else {
+    console.log(chalk.red('No yarn or npm found. Cannot run installation.'));
+  }
+}
 
 export function postProcess(options: ICliOptions) {
   const isNode = fs.existsSync(path.join(options.templatePath, 'package.json'));
   if (isNode) {
-    cd(options.targetPath);
-    exec('npm i');
-    exec('git init');
+    postProcessNode(options);
   }
 }
